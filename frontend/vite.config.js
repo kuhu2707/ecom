d5eb4211f586cc3951309c79050ba8bafbddd85b
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-03 00:32:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  build: {
+    sourcemap: false, // Disable source maps in production
+    minify: 'esbuild',
+    server:{port:5173} // Use esbuild for minification
+  },
+});
